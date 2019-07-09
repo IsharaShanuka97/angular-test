ddc2e3dcccba4e5cfb812811daea4b45c10e369f
Ishara Shanuka <isharashanuka97@gmail.com>
2019-07-09 20:41:14 +0530

    ALL added

---
+++ hello-world/src/app/test/test.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: '[app-test]',
+  template: '<div>Inline</div>',
+  styleUrls: ['./test.component.css']
+})
+export class TestComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
