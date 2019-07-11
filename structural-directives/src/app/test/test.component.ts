cc578ac1ccd1a6cf9d71f1b1ab3f14754eab9a7c
Ishara Shanuka <isharashanuka97@gmail.com>
2019-07-11 22:43:41 +0530

    ngFor Directive

--- structural-directives/src/app/test/test.component.ts
+++ structural-directives/src/app/test/test.component.ts
@@ -3,6 +3,9 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-test',
   template: `
+  <div *ngFor="let color of colors; index as i">
+    <h2> {{i}} {{color}}</h2>
+  </div>
         
               
   `,
@@ -10,7 +13,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TestComponent implements OnInit {
 
-
+  public colors = ["red", "green", "yellow", "blue"];
 
   constructor() { }
 
