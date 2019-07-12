5731970ee4837d256cabe2ee04fc5507d196fea7
Ishara Shanuka <isharashanuka97@gmail.com>
2019-07-12 22:42:39 +0530

    Pipes

--- structural-directives/src/app/test/test.component.ts
+++ structural-directives/src/app/test/test.component.ts
@@ -1,26 +1,46 @@
-import { Component, OnInit, Input,  Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input,  Output,  } from '@angular/core';
 import { EventEmitter } from 'protractor';
 
 @Component({
   selector: 'app-test',
   template: `
-     <h2>{{"Hello " + name}}</h2>
-     <button (click)="fireEvent()">Send codevolution</button>
+     <h2>{{name}}</h2>
+     <h2>{{ name | lowercase }}</h2>
+     <h2> {{ name | uppercase }}</h2>
+     <h2> {{ message | uppercase }}</h2>
+     <h2>{{ message | slice:start:6 }}</h2>
+     <h2> {{ person | json }}</h2>
+
+     <h2>{{5.678 | number:'2.4-10' }}</h2>
+
+     <h2>{{0.25 | percent:digitInfo }}</h2>
+
+     <h2>{{ 100 | currency:'$' }}</h2>
+
+     <h2>{{ date | date }}</h2>
+     <h2> {{ date | date:'fullDate' }}</h2>
+     <h2>{{ date | date:'shortDate' }}</h2>
+
   `,
   styleUrls: ['./test.component.css']
 })
 export class TestComponent implements OnInit {
 
-  @Input('parentData') public name;
-  @Output() public childEvent = new EventEmitter();
+  public name="Ishara";
+  public message =  "Welcome to Codevolution";
+  public person={
+    "firstName" : "Ayesha",
+    "lastName" : "Gamage"
+  }
+
+  public date = new Date();
+
+ 
 
   constructor() { }
 
   ngOnInit() {
   }
 
-  fireEvent(){
-    this.childEvent.emit('Hey Codevolution');
-  }
-
+  
 }
