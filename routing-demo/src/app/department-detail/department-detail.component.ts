74a4c0774f4e268628d0460ae7a49a104c205bc5
Ishara Shanuka <isharashanuka97@gmail.com>
2019-08-08 17:34:42 +0530

    routing demo added

---
+++ routing-demo/src/app/department-detail/department-detail.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+
+@Component({
+  selector: 'app-department-detail',
+  template: `
+    <p>
+      department-detail works! {{departmentId}}
+    </p>
+  `,
+  styles: []
+})
+export class DepartmentDetailComponent implements OnInit {
+
+  public departmentId;
+
+  constructor(private route : ActivatedRoute) { }
+
+  ngOnInit() {
+    let id = parseInt(this.route.snapshot.paramMap.get('id'));
+    this.departmentId = id;
+  }
+
+}
